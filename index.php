ee522038bffb62f9d8ce615f0988c5131fbeef95
carlos <carlos@sobremesa>
2017-03-20 21:51:15 +0100

    #1 -> Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,51 @@
+<html lang="es">
+<head>
+	<title>Cuadrícula 960gs</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/text.css">
+	<link rel="stylesheet" href="css/960_12_col.css">
+</head>
+
+<body>
+<div class="container_12">
+	<div class="grid_12">
+		<p>Cuestionario de Seguimiento.</p>
+                <p>Valoraci&oacute;n Peri&oacute;dica de los Pasos hacia un mejor Estilo de Vida.</p>
+	</div>
+	<div class="grid_9">
+		<p class="azul">Nihil hic munitissimus habendi senatus locus, nihil horum? Quis aute iure reprehenderit in voluptate velit esse. Curabitur blandit tempus ardua ridiculus sed magna. Quam diu etiam furor iste tuus nos eludet?
+		Fabio vel iudice vincam, sunt in culpa qui officia. A communi observantia non est recedendum. Quam temere in vitiis, legem sancimus haerentia. Unam incolunt Belgae, aliam Aquitani, tertiam. Sed haec quis possit intrepidus aestimare tellus. Nihilne te nocturnum praesidium Palati, nihil urbis vigiliae. Quam diu etiam furor iste tuus nos eludet? Nihil hic munitissimus habendi senatus locus, nihil horum? Tu quoque, Brute, fili mi, nihil timor populi, nihil! Curabitur blandit tempus ardua ridiculus sed magna.
+		Ullamco laboris nisi ut aliquid ex ea commodi consequat. Cras mattis iudicium purus sit amet fermentum. Unam incolunt Belgae, aliam Aquitani, tertiam. Pellentesque habitant morbi tristique senectus et netus. A communi observantia non est recedendum. Fictum,  deserunt mollit anim laborum astutumque!</p>
+	</div>
+	<div class="grid_3">
+		<p class="verde">Inmensae subtilitatis, obscuris et malesuada fames. Quam temere in vitiis, legem sancimus haerentia. Ab illo tempore, ab est sed immemorabili. Quo usque tandem abutere, Catilina, patientia nostra?</p>
+	</div>
+	<div class="grid_4">
+		<p class="rosa">Pellentesque habitant morbi tristique senectus et netus. Pellentesque habitant morbi tristique senectus et netus. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ambitioni dedisse scripsisse iudicaretur. Plura mihi bona sunt, inclinet, amari petere vellent.</p>
+	</div>
+	<div class="grid_4">
+		<p class="rosa">Qui ipsorum lingua Celtae, nostra Galli appellantur. Pellentesque habitant morbi tristique senectus et netus. Pellentesque habitant morbi tristique senectus et netus.</p>
+	</div>
+	<div class="grid_4">
+		<p class="rosa">Plura mihi bona sunt, inclinet, amari petere vellent.
+		Excepteur sint obcaecat cupiditat non proident culpa. Tu quoque, Brute, fili mi, nihil timor populi, nihil! Quis aute iure reprehenderit in voluptate velit esse.</p>
+	</div>
+	<div class="clear"></div>
+	<div class="grid_5">
+		<p class="verde">Hi omnes lingua, institutis, legibus inter se differunt. Inmensae subtilitatis, obscuris et malesuada fames. Morbi fringilla convallis sapien, id pulvinar odio volutpat. Idque Caesaris facere voluntate liceret: sese habere.
+		Paullum deliquit, ponderibus modulisque suis ratio utitur. Quisque placerat facilisis egestas cillum dolore. Non equidem invideo, miror magis posuere velit aliquet. Morbi fringilla convallis sapien, id pulvinar odio volutpat. Quis aute iure reprehenderit in voluptate velit esse.</p>
+	</div>
+	<div class="grid_5 prefix_2">
+		<p class="azul">At nos hinc posthac, sitientis piros Afros. Quam temere in vitiis, legem sancimus haerentia. Inmensae subtilitatis, obscuris et malesuada fames. Curabitur blandit tempus ardua ridiculus sed magna.</p>
+	</div>
+	<div class="clear"></div>
+	<div class="grid_6 prefix_3 suffix_3">
+		<p class="rosa">Quis aute iure reprehenderit in voluptate velit esse. Quam diu etiam furor iste tuus nos eludet? Quam diu etiam furor iste tuus nos eludet? Donec sed odio operae, eu vulputate felis rhoncus. Paullum deliquit, ponderibus modulisque suis ratio utitur. Contra legem facit qui id facit quod lex prohibet. Me non paenitet nullum festiviorem excogitasse ad hoc. Quis aute iure reprehenderit in voluptate velit esse. Quid securi etiam tamquam eu fugiat nulla pariatur. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+	</div>
+	<footer class="grid_12">
+		<p class="amarillo">Mercedem aut nummos unde unde extricat, amaras. At nos hinc posthac, sitientis piros Afros.</p>
+	</footer>
+</div>
+</body>
+</html>
